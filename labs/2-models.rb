d841f8a1ed7ba2438ccb51671d8b73cf0cb4b603
Claudia Steck <claudia.steck@kellogg.northwestern.edu>
2024-01-30 17:33:04 +0000

    SQL-Ruby

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -15,22 +15,62 @@ Salesperson.destroy_all
 #   how many rows have been inserted into the salespeople table.
 #   Lastly, if you want a challenge, try writing code to display the
 #   full name of each salesperson.
+#ew_company = Company.new
+#puts new_company.inspect
+
+#new_company["name"]= "Apple"
+#new_company["city"] ="Cupertino"
+#new_company["state"]="California"
+#new_company["url"]="https://www.apple.com"
+#new_company.save
+
+#initialize a new salesperson row (aka hash)
+new_salesperson1 = Salesperson.new
+
+new_salesperson1["first_name"] = "Claudia"
+new_salesperson1["last_name"] = "Steck"
+new_salesperson1["email"] = "claudemail@gmail.com"
+
+new_salesperson1.save
+
+puts new_salesperson1.inspect
 
 # 1a. check out the schema file
 # 1b. check out the model file
 
 # 2. insert 1-2 rows in salespeople table.
+new_salesperson2 = Salesperson.new
+
+new_salesperson2["first_name"] = "Ethan"
+new_salesperson2["last_name"] = "O"
+new_salesperson2["email"] = "ethano@gmail.com"
+
+new_salesperson2.save
+
+puts new_salesperson2.inspect
 
 # 3. write code to display how many salespeople rows are in the database
+
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+#find claudia column
+claudia = Salesperson.find_by({"first_name"=>"Claudia", "last_name" => "Steck"})
+#update email column
+claudia["email"] = "claudsteck@email.com"
+# update aka "save" the row
+claudia.save
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+salespeople = Salesperson.all
+for person in salespeople
+    puts "#{"first_name"} #{"last_name"}"
 
+puts "There are #{Salesperson.all.count} sales people"
+end
 # ---------------------------------
 # Salespeople: 2
 # Ben Block
